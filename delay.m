644f7fda745d7a6c2fb6cb551b45ec703244b270
Mostafa Elhoushi <m.elhoushi@ieee.org>
2018-12-26 12:08:56 -0500

    handle multiple input case

--- delay.m
+++ delay.m
@@ -2,7 +2,7 @@ function [ x1 ] = delay( x, k )
 %DELAY Summary of this function goes here
 %   Detailed explanation goes here
 
-x1 = [zeros(k,1); x(1:end-k)];
+x1 = [zeros(k,size(x,2)); x(1:end-k)];
 
 end
 
